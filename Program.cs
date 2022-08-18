83260ee9c92210ae5cd0744c14145d9b7c321eb2
Ulises Rivera <88818209+UlisesRiveraRiv@users.noreply.github.com>
2022-08-18 15:34:31 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        int mes, dia, año;
+
+        Console.WriteLine("escribe el dia (en numero):");
+        dia = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Escribe el mes(en numero)");
+        mes = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Escribe el año (en numero)");
+        año = Convert.ToInt32(Console.ReadLine());
+
+        if (mes == 12)
+        {
+            if (dia == 25)
+            {
+
+                Console.WriteLine("Feliz Navidad");
+
+            }
+            else
+            {
+                Console.WriteLine("Tomalo con calma");
+            }
+        }
+        else 
+        {
+            Console.WriteLine("Espera un poco mas");
+
+        }
+       
+    }
+}
